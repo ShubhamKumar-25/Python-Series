0072650326969ad2da326626b92d52f29f832cd9
Shubham <sk5989229@gmail.com>
2026-08-16 03:01:02 +0530

    Python login example

--- Python-Course/Day_2.py
+++ Python-Course/Day_2.py
@@ -17,3 +17,17 @@ print(message)
 name = "Shubham"
 age = 23
 print(f"My name is {name} and I am {age} years old.")
+
+# variable ki value change karna 
+city = "Motihari"
+print(city)
+city = "Punjab"
+print(city)
+
+
+print("----------------------------------------------------------")
+
+userName = "Rohit Gupta"
+password = 1234
+print(f"UserName: {userName}" )
+print(f"password: {password}")
