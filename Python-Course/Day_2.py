d79050bbb94858dca35243d2fbbfaa83f56047e7
Shubham <sk5989229@gmail.com>
2026-08-16 02:49:17 +0530

    Variable 2

--- Python-Course/Day_2.py
+++ Python-Course/Day_2.py
@@ -8,3 +8,12 @@ role = "MERN Developer"
 print(name)
 print(age)
 print(role)
+
+name = "Shubham Gupta"       # String
+age = 23               # Integer
+cgpa = 8.2             # Float
+is_student = True      # Boolean
+print(name)
+print(age)
+print(cgpa)
+print(is_student)
