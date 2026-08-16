e16e239baaac1d81d0cba18a7ca588b02d90b2c5
Shubham <sk5989229@gmail.com>
2026-08-17 01:39:38 +0530

    Data types in python

--- Python-Course/Day_1.py
+++ Python-Course/Day_1.py
@@ -1,13 +1,18 @@
-print("Hello World")
-print(4 + 5)
-print("Kaise ho aap sab")
+# print("Hello World")
+# print(4 + 5)
+# print("Kaise ho aap sab")
 
 
-# def chai(n):
-#     print(n)
-# chai(55)
+# # def chai(n):
+# #     print(n)
+# # chai(55)
 
-# for ch in "rohan":
-#     print(ch)
+# # for ch in "rohan":
+# #     print(ch)
 
-print("Hello")
+# print("Hello")
+
+# Data type in python
+age = 23
+marks = 450
+print(type(age))
