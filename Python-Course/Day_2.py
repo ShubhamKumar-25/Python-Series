9287a82cdde4be35fbe2465da8bd634c535f0b88
Shubham <sk5989229@gmail.com>
2026-08-22 00:49:23 +0530

    slice

--- Python-Course/Day_2.py
+++ Python-Course/Day_2.py
@@ -51,7 +51,12 @@
 
 
 # Slicing is python
-str = "Apple"
-print(str[1 : 4])
-print(str[0 : 6])
-print(str[4 : 9])
+# str = "Apple"
+# print(str[1 : 4])
+# print(str[0 : 6])
+# print(str[4 : 9])
+
+str = "Rohan_Gupta"
+print(str[3 : 8])
+print(str[: 6]) # [0 : 6] python assume that it start from zero(0)
+print(str[2: ]) # [2 : len(str)] python assume that it is end with length of the string
