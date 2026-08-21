80869d06ea5b93bdc6cc78f1f64ea31052b14941
Shubham <sk5989229@gmail.com>
2026-08-22 00:30:27 +0530

    String

--- Python-Course/Day_2.py
+++ Python-Course/Day_2.py
@@ -1,33 +1,39 @@
 
 
 # variable in python
-name = "Shubham"
-age = 23
-role = "MERN Developer"
+# name = "Shubham"
+# age = 23
+# role = "MERN Developer"
 
-print(name)
-print(age)
-print(role)
+# print(name)
+# print(age)
+# print(role)
 
-name = "Rohan"
-message = "Hello " + name
-print(message)
+# name = "Rohan"
+# message = "Hello " + name
+# print(message)
 
 
-name = "Shubham"
-age = 23
-print(f"My name is {name} and I am {age} years old.")
+# name = "Shubham"
+# age = 23
+# print(f"My name is {name} and I am {age} years old.")
 
 # variable ki value change karna 
-city = "Motihari"
-print(city)
-city = "Punjab"
-print(city)
+# city = "Motihari"
+# print(city)
+# city = "Punjab"
+# print(city)
 
 
-print("----------------------------------------------------------")
+# print("----------------------------------------------------------")
 
-userName = "Rohit Gupta"
-password = 1234
-print(f"UserName: {userName}" )
-print(f"password: {password}")
+# userName = "Rohit Gupta"
+# password = 1234
+# print(f"UserName: {userName}" )
+# print(f"password: {password}")
+
+
+
+# String In Python
+str = "Hey Everryone Rohan Gupta this side.\nWhat about you?"
+print(str)
