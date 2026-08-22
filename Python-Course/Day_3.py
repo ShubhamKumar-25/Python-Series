60d3df0f32e068d7a9f4301b83faa252cda00293
Shubham <sk5989229@gmail.com>
2026-08-23 01:19:25 +0530

    dist

--- Python-Course/Day_3.py
+++ Python-Course/Day_3.py
@@ -1,9 +1,19 @@
+# info = {
+#     "Name": "Rohan Gupta",
+#     "age": 22,
+#     "is_student": True,
+#     "city": "Motihari",
+#     "Salary": 30000
+# }
+
+# print(info)
+
 info = {
     "Name": "Rohan Gupta",
-    "age": 22,
-    "is_student": True,
-    "city": "Motihari",
-    "Salary": 30000
+    "Subjects": ["JAVA", "C++", "PYTHON", "JAVASCRIPT"],
+    "Topic": ("Dist", "Set"),
+    "Marks": 89.0,
+    "is_Student": True,
+    "City": "Motihari"
 }
-
 print(info)
