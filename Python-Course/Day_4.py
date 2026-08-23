381f05e94db3367e91df29e9b899a0c6b3683065
Shubham <sk5989229@gmail.com>
2026-08-24 01:45:32 +0530

    looping

--- Python-Course/Day_4.py
+++ Python-Course/Day_4.py
@@ -5,5 +5,21 @@
 #     count += 1
 
 
-while True:
-    print("Running forever.............")
+# while True:
+#     print("Running forever.............") This bad looping
+
+
+# i = 1
+# while i < 5:
+#     if i == 3:
+#         break
+#     print(i)
+#     i += 1
+
+
+i = 1
+while i < 10:
+    print(i)
+    i += 1
+else:
+    print("Loop is end")
