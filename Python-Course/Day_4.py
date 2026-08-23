58ce6a2b2bd6aa62967a31918b4101c0e1190cae
Shubham <sk5989229@gmail.com>
2026-08-24 01:40:47 +0530

    bad loop

--- Python-Course/Day_4.py
+++ Python-Course/Day_4.py
@@ -1,5 +1,9 @@
 
-count = 0
-while count < 5:
-    print("Hello Rohan Gupta")
-    count += 1
+# count = 0
+# while count < 5:
+#     print("Hello Rohan Gupta")
+#     count += 1
+
+
+while True:
+    print("Running forever.............")
