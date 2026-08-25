11bd02942d915c54e536ea3f9030e85dcd4ce28f
Shubham <sk5989229@gmail.com>
2026-08-26 01:59:57 +0530

    file in

--- Python-Course/Day_5.py
+++ Python-Course/Day_5.py
@@ -6,5 +6,12 @@ file = open("Student_records.txt", 'w')
 file.write("Name: Shubham\n")
 file.write("Course: B.Tech IT\n")
 file.write("Status: Active\n")
+file.write("Marks: 89\n")
+file.write("City: Motihari\n")
 file.close()
 print("File successfully created and data written!")
+
+
+with open("Student_records.txt", 'a') as file:
+    file.write("I am fullstack software develpoer \n")
+print("New Data append sucessfully")
