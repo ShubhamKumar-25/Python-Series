18f8e2315b16b805828132aa40c5aedfe229ab3d
Shubham <sk5989229@gmail.com>
2026-08-27 03:02:08 +0530

    File

--- Python-Course/Day_5.py
+++ Python-Course/Day_5.py
@@ -1,23 +1,51 @@
 
-# File input and output in python
-file = open("Student_records.txt", 'w')
+# # File input and output in python
+# file = open("Student_records.txt", 'w')
 
-# Data write karna
-file.write("Name: Shubham\n")
-file.write("Course: B.Tech IT\n")
-file.write("Status: Active\n")
-file.write("Marks: 89\n")
-file.write("City: Motihari\n")
-file.close()
-print("File successfully created and data written!")
-
-
-with open("Student_records.txt", 'a') as file:
-    file.write("I am fullstack software develpoer \n")
-print("New Data append sucessfully")
+# # Data write karna
+# file.write("Name: Shubham\n")
+# file.write("Course: B.Tech IT\n")
+# file.write("Status: Active\n")
+# file.write("Marks: 89\n")
+# file.write("City: Motihari\n")
+# file.close()
+# print("File successfully created and data written!")
 
 
-with open("Student_records.txt", "r") as file:
-    content = file.read()
-    print("-----Read file--------")
-    print(content)
+# with open("Student_records.txt", 'a') as file:
+#     file.write("I am fullstack software develpoer \n")
+# print("New Data append sucessfully")
+
+
+# with open("Student_records.txt", "r") as file:
+#     content = file.read()
+#     print("-----Read file--------")
+#     print(content)
+
+
+
+
+
+
+from datetime import datetime
+
+def log_activity(username, action):
+    # Current date and time fetch karna
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Formatted log entry
+    log_entry = f"[{current_time}] USER: {username} | ACTION: {action}\n"
+    
+    # Append mode 'a' use karenge taki purana log save rahe
+    with open("app_log.txt", "a") as log_file:
+        log_file.write(log_entry)
+
+# Log test cases
+log_activity("Shubham", "Logged In")
+log_activity("Shubham", "Executed Python Script")
+log_activity("Shubham", "Logged Out")
+
+# Ab logs check karte hain
+with open("app_log.txt", "r") as log_file:
+    print("\n--- Recent Logs ---")
+    print(log_file.read())
