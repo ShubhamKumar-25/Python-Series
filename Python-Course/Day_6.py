ee94e22082926ede8785181347a4c8094e469f9f
Shubham <sk5989229@gmail.com>
2026-08-28 03:08:59 +0530

    inhe

--- Python-Course/Day_6.py
+++ Python-Course/Day_6.py
@@ -4,10 +4,26 @@
 # print(s1.name)
 
 
-class Car:
-    name = "BMW"
-    color = "Black"
-    model = "i1220"
-car1 = Car()
-print(car1.name)
-print(car1.color)
+# class Car:
+#     name = "BMW"
+#     color = "Black"
+#     model = "i1220"
+# car1 = Car()
+# print(car1.name)
+# print(car1.color)
+
+
+
+# Parent Class
+class Animal:
+    def eat(self):
+        print("Eating food...")
+
+# Child Class (Animal ko inherit kar rahi hai)
+class Dog(Animal):
+    def bark(self):
+        print("Bhow Bhow!")
+
+my_dog = Dog()
+my_dog.eat()   # Parent ka feature use kiya
+my_dog.bark()  # Apna feature use kiya
