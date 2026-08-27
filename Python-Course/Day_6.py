3be3cd0d7b387e4c6cbcd3c9d86335e2ee7833ad
Shubham <sk5989229@gmail.com>
2026-08-28 02:45:33 +0530

    oop

--- Python-Course/Day_6.py
+++ Python-Course/Day_6.py
@@ -1,4 +1,13 @@
-class Student1:
-    name = "Rohan Gupta"
-s1 = Student1()
-print(s1.name)
+# class Student1:
+#     name = "Rohan Gupta"
+# s1 = Student1()
+# print(s1.name)
+
+
+class Car:
+    name = "BMW"
+    color = "Black"
+    model = "i1220"
+car1 = Car()
+print(car1.name)
+print(car1.color)
