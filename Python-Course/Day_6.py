41368b8677f26edea4f0777ed86e49d2f2dce06e
Shubham <sk5989229@gmail.com>
2026-08-29 01:56:16 +0530

    Constructor

--- Python-Course/Day_6.py
+++ Python-Course/Day_6.py
@@ -14,16 +14,29 @@
 
 
 
-# Parent Class
-class Animal:
-    def eat(self):
-        print("Eating food...")
+# # Parent Class
+# class Animal:
+#     def eat(self):
+#         print("Eating food...")
 
-# Child Class (Animal ko inherit kar rahi hai)
-class Dog(Animal):
-    def bark(self):
-        print("Bhow Bhow!")
+# # Child Class (Animal ko inherit kar rahi hai)
+# class Dog(Animal):
+#     def bark(self):
+#         print("Bhow Bhow!")
 
-my_dog = Dog()
-my_dog.eat()   # Parent ka feature use kiya
-my_dog.bark()  # Apna feature use kiya
+# my_dog = Dog()
+# my_dog.eat()   # Parent ka feature use kiya
+# my_dog.bark()  # Apna feature use kiya
+
+
+
+
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        print("Adding a new Element in database...")
+
+s1 = Student("Rahul", 89)
+print(s1.name, s1.marks)
+
