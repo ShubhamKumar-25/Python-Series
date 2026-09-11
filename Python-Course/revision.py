fea197083df9c7815a4f96104565108eb8ddf093
Shubham <sk5989229@gmail.com>
2026-09-12 02:03:33 +0530

    re

--- Python-Course/revision.py
+++ Python-Course/revision.py
@@ -1,9 +1,14 @@
-print("Hey Rohan Gupta")
+# print("Hey Rohan Gupta")
 
-print("Kya hal hai bhai log")
+# print("Kya hal hai bhai log")
 
-name = "Rohan Kumar"
-print(name)
+# name = "Rohan Kumar"
+# print(name)
 
-marks = 98
-print(marks)
+# marks = 98
+# print(marks)
+
+
+name = "Shubham Kumar"
+age = 22
+print(name, age)
