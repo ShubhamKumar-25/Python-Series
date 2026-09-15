8b41c920022f147e4a16fe155e47e786c622d504
Shubham <sk5989229@gmail.com>
2026-09-16 00:47:50 +0530

    oops

--- Python-Course/oops.py
+++ Python-Course/oops.py
@@ -54,3 +54,21 @@ dog.bark()
 cat = Cat()
 cat.eat()
 cat.meow()
+
+
+print("========================================")
+
+class Camera:
+    def take_photo(self):
+        print("Taking a photo...")
+
+class musicPlayer:
+    def play_music(self):
+        print("Playing music...")
+
+class smartPhone2(Camera, musicPlayer):
+    pass
+
+sp2 = smartPhone2()
+sp2.take_photo()
+sp2.play_music()
